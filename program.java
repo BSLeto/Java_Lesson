f1aa23d2ce40a4a1294c7a223ae9913f9147d946
Leto <mikhaylichenko2000@bk.ru>
2022-11-30 11:54:27 +0300

    Init

---
+++ program.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import javax.swing.JTable.PrintMode;
+
+public class program {
+   public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    System.out.println(LocalDateTime.now().getHour());
+   }
+}
+    
